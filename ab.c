9c8d91f9ab3213fc3db285733b0a956b0f94a0fb
Anton Jusko <Anton.Jusko@cern.ch>
2015-04-03 12:26:36 +0200

    first

---
+++ ab.c
@@ -0,0 +1,3 @@
+ delete mode 100644 monscal_root/WORK/gmonscal1205021427.log
+ delete mode 100644 monscal_root/WORK/gmonscal1205021635.log
+
---
+++ ab2.c
@@ -0,0 +1,3 @@
+delete mode 100644 monscal_root/WORK/gmonscal1205021644.log
+ delete mode 100644 monscal_root/WORK/gmonscal1205021650.log
+
